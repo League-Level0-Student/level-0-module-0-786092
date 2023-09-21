4652bcbcd106588fe2a2847de5302da4a520ac1f
root <root@71a373454d9c>
2023-09-21 01:21:33 +0000

    Awesome or Not

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -25,14 +25,14 @@ public class MadlibsOfTheAmazon {
 		
 		String story = 
 		"Piranhas are more "+adjective+" during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"night. Piranhas are attracted to fresh "+liquid+" and will most\n"  +
+		"likely take a bite out of your "+part+" if you "+verb+". Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the "+place+". Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
-		
+		JOptionPane.showMessageDialog(null, story);
 		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,18 +1,55 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score=0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+					String question1 = JOptionPane.showInputDialog("How many championships do the Los Angeles Lakers have? ");
 				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
+					if(question1.equals("17")) {
+						score+=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+					}
+					else {
+						score-=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+					}
+					String question2 = JOptionPane.showInputDialog("What is the most legendary rivalry in basketball?: ");
+					if(question2.equals("Los Angeles Lakers vs Boston Celtics")) {
+						score+=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+								
+					}
+					else {
+						score-=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+					}
+					String question3 = JOptionPane.showInputDialog("Who leads the Lakers in points? ");
+					if(question3.equals("Kobe Bryant")) {
+						score+=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+					}
+					else {
+						score-=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+					}
+					String question4 = JOptionPane.showInputDialog("What was the Lakers dynasty in the 1980s called? ");
+					if(question4.equals("Showtime Lakers")) {
+						score+=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+					}
+					else {
+						score-=1;
+						JOptionPane.showMessageDialog(null, "Score: "+score);
+					}
+					// 4.  if the user's answer was correct, add one to their score 
+					
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
